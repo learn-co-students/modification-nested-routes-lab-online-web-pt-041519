858d20b4759b0cfc32382c7068cc484cd357ea50
aparkening <aaron.parkening@gmail.com>
2019-09-24 17:47:10 -0400

    Update artists_helper to include hidden form and separate display_name method.

--- app/helpers/artists_helper.rb
+++ app/helpers/artists_helper.rb
@@ -6,10 +6,18 @@ module ArtistsHelper
   # Display dropdown of artists, with name if editing through nested
   def artist_select(song, path)
     if song.artist && path == "nested"
-      song.artist.name
+      hidden_field_tag "song[artist_id]", song.artist_id
     else 
       select_tag "song[artist_id]", options_from_collection_for_select(Artist.all, :id, :name)
     end
   end
+
+  # Display name if editing through nested
+  def display_name(song, path)
+    if song.artist && path == "nested"
+      song.artist.name
+    end
+  end 
+
 
 end
